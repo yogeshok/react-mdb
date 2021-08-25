04b67340722bc46428d1532dab0f728073b45ecc
Yogesh Kumar <yogiky2002@gmail.com>
2021-08-26 01:37:17 +0530

    Fetch data from API

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+//API
+import API from '../API';
 
 // Config
-import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL, API_KEY } from '../config';
 
 // Component
 
@@ -16,6 +18,32 @@ const Home = () => {
     const [state, setState] = useState();
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
+
+    const fetchMovies = async (page, searchTerm = "") => {
+        try {
+            setError(false);
+            setLoading(true);
+
+            const movies = await API.fetchMovies(searchTerm, page);
+
+            // () is used to return a object
+            setState(prev => ({
+                ...movies,
+                results:
+                    page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
+            }))
+
+        } catch (error) {
+            setError(true);
+        }
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        fetchMovies(1);
+    }, [])
+
+    console.log(state);
 
     return (
         <div>Home Page</div>
--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 // Read more about the API here: https://developers.themoviedb.org/
 
 const API_URL = 'https://api.themoviedb.org/3/';
-const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = '8b917111643f3a5aea02c4db8d4b1de5';
 
 const SEARCH_BASE_URL = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=`;
 const POPULAR_BASE_URL = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US`;
