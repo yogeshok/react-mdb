4128ed42fa10a69285b212956640e284807d5c0c
Yogesh Kumar <yogiky2002@gmail.com>
2021-09-03 23:30:02 +0530

    Button component added

---
+++ src/Components/Button/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//styles
+import { Wrapper } from './Button.styles';
+
+const Button = ({ text, callback}) => (
+    
+    <Wrapper type='button' onclick={callback}>
+        {text}
+    </Wrapper>
+);
+
+export default Button;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -11,6 +11,7 @@ import Grid from './Grid';
 import Thumb from './Thumb';
 import Spinner from './Spinner';
 import SearchBar from './SearchBar';
+import Button from './Button';
 
 // Hook
 import { useHomeFetch } from '../Hooks/useHomeFetch';
@@ -48,7 +49,12 @@ const Home = () => {
                     />
                 ))}
             </Grid>
-            <Spinner />
+            {
+            loading && <Spinner />}
+
+            {state.page < state.total_pages && !loading && (
+                <Button text='Load More' /> 
+            )}
         </>
     )
 }
