d8d4ec2c90c8eaa032b78a943e4722bb78f0a16e
Yogesh Kumar <yogiky2002@gmail.com>
2021-09-11 23:31:12 +0530

    added component

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 //config
 import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
 //components
+import BreadCrumb from "./BreadCrumb";
 import Grid from "./Grid";
 import Spinner from "./Spinner";
 //Hook
@@ -15,10 +16,12 @@ const Movie = () => {
     
     const { state: movie, loading, error } = useMovieFetch(movieId);
 
-    console.log(movie);
+    if(loading) return <Spinner />
+    if(error) return <div>Something went wrong...</div>
+
     return (
         <>
-        <div>Movie</div>
+            <BreadCrumb movieTitle={movie.original_title} />
         </>
     )
 };
