93744678d2fb669573903e61a63893ae3cef014a
Yogesh Kumar <yogiky2002@gmail.com>
2021-09-16 23:57:41 +0530

    styles added

--- src/Components/Actor/Actor.styles.js
+++ src/Components/Actor/Actor.styles.js
@@ -1,5 +1,25 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+    color: var(--white);
+    background: var(--darkGrey);
+    border-radius: 20px;
+    padding: 2px;
+    text-align: center;
 
-export const Image = styled.img``;
+    h3 {
+        margin: 8px 0 0 0;
+    }
+
+    p {
+        margin: 4px 0;
+    }
+`;
+
+export const Image = styled.img`
+    display: block;
+    width: 100%;
+    height: 200px;
+    object-fit: cover;
+    border-radius: 15px;
+`;
