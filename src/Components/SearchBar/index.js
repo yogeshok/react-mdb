593a09ac28fe1183b1c25e3397e5774039cbdf16
Yogesh Kumar <yogiky2002@gmail.com>
2021-10-04 23:54:28 +0530

    adding class components

--- src/Components/SearchBar/index.js
+++ src/Components/SearchBar/index.js
@@ -1,41 +1,63 @@
-import React, { useState, useEffect, useRef } from 'react';
+//import React, { useState, useEffect, useRef } from 'react';
+//class comonents
+import React, { Component } from 'react';
+
 import PropTypes from 'prop-types';
 // Image
 import searchIcon from '../../images/search-icon.svg';
 // Styles
 import { Wrapper, Content } from './SearchBar.styles';
 
-const SearchBar = ({ setSearchTerm }) => {
-    const [state, setState] = useState('');
+class SearchBar extends Component {
+    // const [state, setState] = useState('');
 
-    const initial = useRef(true);
+    // const initial = useRef(true);
+
+    state = {value:''};
+    timeout = null;
     // actual value is in initial.current
-
-    useEffect(() => {
-        if(initial.current){
-            initial.current = false;
-            return;
-        }
+    // useEffect(() => {
+    //     if(initial.current){
+    //         initial.current = false;
+    //         return;
+    //     }
         
-        const timer = setTimeout(() => {
-            setSearchTerm(state);
-        }, 400)
-        return() => clearTimeout(timer)
-    }, [setSearchTerm, state])
+        // const timer = setTimeout(() => {
+        //     setSearchTerm(state);
+        // }, 500)
+    //     return() => clearTimeout(timer)
+    // }, [setSearchTerm, state])
 
-    return (
-        <Wrapper>
-            <Content>
-                <img src={searchIcon} alt='search-icon' />
-                <input
-                    type='text'
-                    placeholder='Search Movie'
-                    onChange={event => setState(event.currentTarget.value)}
-                    value={state}
-                />
-            </Content>
-        </Wrapper>
-    )
+    componentDidUpdate(_prevProps, prevState) {
+        if(this.state.value !== prevState.value) {
+            const {setSearchTerm} = this.props;
+
+            clearTimeout(this.timeout);
+
+            this.timeout = setTimeout(() => {
+                const { value } = this.state;
+                setSearchTerm(value);
+            }, 500)
+        }
+    }
+
+    render() {
+        return (
+            <Wrapper>
+                <Content>
+                    <img src={searchIcon} alt='search-icon' />
+                    <input
+                        type='text'
+                        placeholder='Search Movie'
+                        onChange={event => this.setState({value: event.currentTarget.value})}
+                        value={this.state.value}
+                    />
+                </Content>
+            </Wrapper>
+        )
+    }
+
+    
 }
 
 SearchBar.propTypes = {
